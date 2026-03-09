ae07af2e5e76b61408c1490d443bdf7b350751af
Jeslyn-331 <ykjeslyn@1utar.my>
2026-03-10 01:07:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Medi-Flow Orchestrator")
 transcript = st.text_area("Doctor Transcript")
 
 if st.button("Analyze"):
-    st.write("AI detected123:")
+    st.write("AI detected hahahah:")
     st.write("Blood Test → Laboratory")
     st.write("Paracetamol → Pharmacy")
