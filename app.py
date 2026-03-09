25275de5a3bdaee0f891ab279cb8743a4a975922
Jeslyn-331 <ykjeslyn@1utar.my>
2026-03-10 00:47:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-import streamlit
+import streamlit as st
+
+st.title("Medi-Flow Orchestrator")
+
+transcript = st.text_area("Doctor Transcript")
+
+if st.button("Analyze"):
+    st.write("AI detected:")
+    st.write("Blood Test → Laboratory")
+    st.write("Paracetamol → Pharmacy")
